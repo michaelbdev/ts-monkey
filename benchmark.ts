414d57aa6218df84dfdd905d958db21106f005b6
Michael <michaelb199999@gmail.com>
2025-03-03 02:17:09 +0000

    tidy up input for benchmark

--- benchmark.ts
+++ benchmark.ts
@@ -7,17 +7,12 @@ import { Parser } from "./src/parser/parser";
 import { VM } from "./src/vm/vm";
 
 const input = `let fibonacci = fn(x) {
-    if (x == 0) {
-    0
-    } else {
-    if (x == 1) {
-    return 1;
-    } else {
-    fibonacci(x - 1) + fibonacci(x - 2);
-    }
-    }
-    };
-    fibonacci(35);`;
+  if (x <= 1) {
+    return x;
+  }
+  return fibonacci(x - 1) + fibonacci(x - 2);
+}
+fibonacci(35);`;
 
 const engine = process.argv
 	.find((a) => a.startsWith("--engine="))
